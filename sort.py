39c0397e148b33d6f504549d3212da5dd365c66c
Khsammy <36905832+Khsammy@users.noreply.github.com>
2020-11-01 15:24:54 +0100

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,8 +1,4 @@
 #sorting algorithms in python
-# quicksort, mergesort
-#parameters ==> type->e.g heap,quick,merge | list->e.g [1,2,3,5]i
-#usage python sort.py --sort-type quicksort --items '67,89,12,56,72,14,89,16'
-# for single digits like 1,2,3,4 use the format 01,02,03,04,05
 
 import time
 import argparse
